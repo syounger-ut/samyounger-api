ca75ac2a78172ecddc23a8d97cd647260ab22055
samy <sam.younger@gmail.com>
2017-09-26 18:40:39 +0100

    authentication completed

--- lib/auth.rb
+++ lib/auth.rb
@@ -2,9 +2,19 @@ require 'jwt'
 
 
 class Auth
-  def self.issue
+  ALGORITHM = 'HS256'
+
+  def self.issue(payload, expiry_in_minutes=60*24*30)
+    payload[:exp] = expiry_in_minutes.minutes.from_now.to_i
+    JWT.encode(payload, auth_secret, ALGORITHM)
   end
 
-  def self.decode
+  def self.decode(token, leeway=0)
+    decoded = JWT.decode(token, auth_secret, true, { leeway: leeway, algorithm: ALGORITHM })
+    HashWithIndifferentAccess.new(decoded[0])
+  end
+
+  def self.auth_secret
+    ENV["AUTH_SECRET"]
   end
 end
