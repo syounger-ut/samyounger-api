53572a8b95abfe74a4ba67c2506e7adc404b362b
samy <sam.younger@gmail.com>
2018-12-24 13:00:36 +0000

    delete action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,17 @@ class UsersController < ApplicationController
 
   def update
     current_user.update(user_params)
-    render json: { token: token, user: UserSerializer.new(current_user, avatar_url: url_for(current_user.avatar)) }, status: :ok
+    render json: {
+      token: token,
+      user: UserSerializer.new(
+        current_user, avatar_url: url_for(current_user.avatar)
+        )
+      }, status: :ok
+  end
+
+  def destroy
+    current_user.destroy
+    render body: nil, status: :no_content
   end
 
   private
