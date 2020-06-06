83a24ebe9235510a7c82fc71c714e7dedd83af2d
samyounger <sam.younger@gmail.com>
2020-06-06 12:20:20 +0100

    remove /api route scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  scope :api do
-    get "/", to: proc { [200, {}, [""]] }
-    resources :users do
-      collection do
-        get "me"
-      end
+  get "/", to: proc { [200, {}, [""]] }
+  resources :users do
+    collection do
+      get "me"
     end
-    post "register", to: "authentications#register"
-    post "login", to: "authentications#login"
   end
+  post "register", to: "authentications#register"
+  post "login", to: "authentications#login"
 end
