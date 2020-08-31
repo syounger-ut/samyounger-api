637b687063b706b66be2f752678024a56a1beb04
samyounger <sam.younger@gmail.com>
2020-08-31 20:50:51 +0100

    require user first_name and last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
 
   # Validations
   validates :email, presence: true, uniqueness: true
+  validates :first_name, :last_name, presence: true
 
   def full_name
     self.first_name + " " + self.last_name
